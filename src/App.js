b8141885668d779cb03dfdce5e8f85b119803c5d
Partheeban Bharati <b.partheeban@hotmail.com>
2020-10-16 23:09:51 -0700

    Added basic API integration

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
+import { render } from '@testing-library/react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Recipe from './Recipe'
+require('dotenv').config()
+const request = require('request');
 
-function App() {
+const App = () => {
+
+
+  const [recipes, setRecipes] = useState([]);
+  const apiKey = process.env.REACT_APP_APIKEY
+
+  const options = {
+    method: 'GET',
+    url: 'https://rapidapi.p.rapidapi.com/recipes/searchComplex',
+    qs: {
+      offset: '0',
+      number: '2',
+      query: 'burger',
+      cuisine: 'american',
+      includeIngredients: 'onions, lettuce, tomato',
+      excludeIngredients: 'coconut, mango',
+      intolerances: 'peanut, shellfish',
+      type: 'main course',
+      ranking: '2',
+      minCalories: '150',
+      maxCalories: '1500',
+      minFat: '5',
+      maxFat: '100',
+      minProtein: '5',
+      maxProtein: '100',
+      minCarbs: '5',
+      maxCarbs: '100',
+      minCholesterol: '0',
+      maxCholesterol: '1000',
+
+    },
+    headers: {
+      'x-rapidapi-host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+      'x-rapidapi-key': apiKey,
+      useQueryString: true
+    }
+  }
+
+  useEffect(() => {
+    getRecipes();
+  }, [])
+
+  const getRecipes = () => {
+    return request(options, function (error, response, body) {
+      if (error) throw new Error(error);
+      // setRecipes(response.toJSON.results)
+      const res = JSON.parse(body);
+      setRecipes(res.results);
+      console.log(res.results[0].title);
+  })
+}
+  
+
+  // const getRecipes = async() => {
+  //   const response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=3&apiKey=` + apiKey)
+  //   const data = await response.json()
+  //   setRecipes(data.results)
+  // }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Text and stuff
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
+      {recipes.map(recipe => (
+        <Recipe title={recipe.title} calories={recipe.calories} image={recipe.image}/>
+      ))}
+
     </div>
   );
-}
+  
+      }
 
 export default App;
